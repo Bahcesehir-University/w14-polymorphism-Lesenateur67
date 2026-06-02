2ac649549fd5e7439c4cf13d30811645add3488e
Lesenateur67 <aime.ackey@epita.fr>
2026-06-02 12:28:56 +0300

    Update MainProgram.cpp

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -1,4 +1,4 @@
-// Lab: Inheritance and // ============================================================
+// ============================================================
 // MainProgram.cpp  —  STUDENT VERSION
 // Lab: Inheritance and Polymorphism
 // Standard: C++17  |  Single file only, no headers
@@ -95,7 +95,7 @@ public:
     //   - Then set name = "Square".
     Square(double side) : Rectangle(side,side) /* : ... */ {
         // TODO
-        name= " Square";
+        name= "Square";
     }
     // Note: Square reuses Rectangle::area() — no need to rewrite it.
 };
